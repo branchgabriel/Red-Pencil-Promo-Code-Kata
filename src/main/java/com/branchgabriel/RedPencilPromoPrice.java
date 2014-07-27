aa8552eb2d00771e20959f70672368f95bf485f8
gabriel <gbranch@everesttech.com>
2014-07-27 16:05:05 -0400

    we can now init the object with a price history map

--- src/main/java/com/branchgabriel/RedPencilPromoPrice.java
+++ src/main/java/com/branchgabriel/RedPencilPromoPrice.java
@@ -1,12 +1,20 @@
 package com.branchgabriel;
 
+import java.util.*;
+
 public class RedPencilPromoPrice{
     private float price = 0.0f;
+    private Map<Date, Float> priceHistory;
 
-    public RedPencilPromoPrice(){
+    public RedPencilPromoPrice(Map<Date, Float> priceHistory) {
+        this.priceHistory = priceHistory;
     }
 
     public float getPrice(){
         return price;
+    }
+
+    public Map<Date, Float> getPriceHistory() {
+        return priceHistory;
     }
 }
--- src/test/java/com/branchgabriel/RedPencilPromoPriceTest.java
+++ src/test/java/com/branchgabriel/RedPencilPromoPriceTest.java
@@ -1,8 +1,11 @@
 package com.branchgabriel;
 
-import static com.branchgabriel.RedPencilPromoAssertions.assertThat;
+import static java.util.Calendar.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.*;
+
+import java.util.*;
 
 public class RedPencilPromoPriceTest{
 
@@ -10,12 +13,35 @@ public class RedPencilPromoPriceTest{
 
     @Before
     public void setup(){
-        redPencilPromoPrice = new RedPencilPromoPrice();
+        redPencilPromoPrice = new RedPencilPromoPrice(buildPriceHistory());
     }
 
     @Test
     public void redPencilPromoInitializesWithZeroBasedPrice(){
-        assertThat(redPencilPromoPrice).hasPrice(0);
+        RedPencilPromoAssertions.assertThat(redPencilPromoPrice).hasPrice(0);
+    }
+
+    @Test
+    public void redPencilPromoInitializedWithPriceHistoryOfItem(){
+        redPencilPromoPrice = new RedPencilPromoPrice(buildPriceHistory());
+        assertThat(redPencilPromoPrice.getPriceHistory()).hasSize(4);
+    }
+
+    private Map<Date, Float> buildPriceHistory() {
+        HashMap<Date, Float> priceHistory = new HashMap<Date, Float>();
+
+        priceHistory.put(createNewDateBasedOnDaysAgo(31), 100.0f);
+        priceHistory.put(createNewDateBasedOnDaysAgo(30), 100.0f);
+        priceHistory.put(createNewDateBasedOnDaysAgo(29), 100.0f);
+        priceHistory.put(createNewDateBasedOnDaysAgo(1), 100.0f);
+
+        return priceHistory;
+    }
+
+    private Date createNewDateBasedOnDaysAgo(int daysAgo){
+        Calendar calendar = getInstance();
+        calendar.add(DATE, (-1*daysAgo));
+        return calendar.getTime();
     }
 
 }
